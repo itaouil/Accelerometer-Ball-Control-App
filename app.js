dbeda14ed7cde168e45c7d9c40912d6e7442c545
Ilyass Taouil <itaouil95@gmail.com>
2017-06-08 20:46:11 +0200

    Added landing page.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 // Dependencies
 const pug     = require('pug');
 const express = require('express');
+const random  = require('./helpers/random');
 
 // Express App
 const app = express();
 
 // Port Serving On
-const port = 3000;
+const port = 8000;
 
 // Start Server
 app.listen(port, () => {
   console.log(`Server listening on port ${port} ...`);
+});
+
+// Route
+app.get("/", (req, res) => {
+
+  try {
+    res.send(pug.compileFile(__dirname + '/templates/game.pug')({
+      id: random(5),
+      title: 'Game',
+      host: req.hostname,
+      port: port
+    }));
+  }
+
+  catch (e) {
+    console.log(e);
+  }
+
 });
